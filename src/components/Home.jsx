f8be671cf9b06614926e58b50abec2e6012fae2a
Arno luyckx <A.luyckx@protonmail.com>
2023-04-10 12:46:22 +0200

    Mapped cards on a grid from the response

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,9 +13,40 @@ import {
 const CLIENT_ID = "3b8c5cfddbc046f693f29ea1c71060bf";
 const CLIENT_SECRET = "bc8877657c0949ceb9414f7a5bacb1ab";
 
+const Artist = ({ artist }) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={artist.image} />
+      <Card.Body>
+        <Card.Title>{artist.name}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+const Grid = ({ artists }) => {
+  return (
+    <Container>
+      <Row>
+        {artists.map((artist, index) => (
+          <Col key={index}>
+            <Artist artist={artist} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
 const Home = () => {
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
+  const [artists, setArtists] = useState([]);
 
   // ? This useEffect is used to get the access token from the spotify api
   useEffect(() => {
@@ -30,8 +61,6 @@ const Home = () => {
       });
       const data = await response.json();
       setAccessToken(data.access_token);
-      console.log(data);
-      console.log(accessToken);
     };
     getAccessToken();
   }, []);
@@ -48,19 +77,26 @@ const Home = () => {
       }
     );
     const data = await response.json();
-    console.log(data);
+    setArtists(
+      data.artists.items.map((item) => ({
+        id: item.id,
+        name: item.name,
+        image:
+          item.images.length > 0
+            ? item.images[0].url
+            : "placeholder.js/100px180", // Use a placeholder image if no image is available
+      }))
+    );
   }
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       handleSearch();
-      console.log(`You pressed the enter key! searchInput: ${searchInput}`);
     }
   };
 
   const handleClick = () => {
     handleSearch();
-    console.log(`You clicked the button! searchterm ${searchInput}`);
   };
 
   return (
@@ -70,7 +106,7 @@ const Home = () => {
           <h1>Home Spotify searcher Altcademy</h1>
           <InputGroup className="mb-3">
             <FormControl
-              placeholder="Search for a artist"
+              placeholder="Search for an artist"
               type="input"
               onKeyPress={handleKeyPress}
               value={searchInput}
@@ -80,6 +116,7 @@ const Home = () => {
           </InputGroup>
         </Col>
       </Row>
+      <Grid artists={artists} />
     </Container>
   );
 };
