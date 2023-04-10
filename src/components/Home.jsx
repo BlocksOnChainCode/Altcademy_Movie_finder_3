6071d6b6437dbbe4bcbefa88e8d2d5d4ec38d29b
Arno luyckx <A.luyckx@protonmail.com>
2023-04-10 10:47:22 +0200

    Base Routes and BrowserRouter set up with V6

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Movie Finder</h1>
+      <p>Search for your favorite movies</p>
+      <input type="text" />
+    </div>
+  );
+};
+
+export { Home };
